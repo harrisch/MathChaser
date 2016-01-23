a49d26ad35a3b44c511d0eb0bce058c266652542
Harris <harrischu@gmail.com>
2016-01-23 15:12:27 -0800

    Moving some responsibility over to the game controller

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
     private int points = 0;
     public Text pointsLabel;
+
+    public MathController mathController;
     
 	// Use this for initialization
 	void Start () {
-        pointsLabel.text = points.ToString(); ;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        pointsLabel.text = points.ToString();
+                // Use this for initialization        mathController.setupQuestion();
+
+    }
+
+    public IEnumerator resetLevel()    {        yield return new WaitForSeconds(1);        mathController.setupQuestion();    }
+
+    // Update is called once per frame
+    void Update () {
         pointsLabel.text = points.ToString();
 	}
     
